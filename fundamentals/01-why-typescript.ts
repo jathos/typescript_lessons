67390248aab6d964f9a8ef5046f59ec26730ee42
jbellon <josh@joshbellon.com>
2022-12-21 15:53:26 -0800

    finished first couple lessons

---
+++ fundamentals/01-why-typescript.ts
@@ -0,0 +1,9 @@
+const lessonName = "Typescript Bootcamp";
+
+printCourseName(lessonName);
+
+function printCourseName(name: string) {
+
+    console.log("The course name is ", name.toUpperCase());
+
+}
