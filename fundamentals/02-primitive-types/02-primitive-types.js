1c3dda465c91b80a8e9350078b1a1f09acceee17
jbellon <josh@joshbellon.com>
2022-12-21 16:40:28 -0800

    finished lesson 'Typescript primitive types'

---
+++ fundamentals/02-primitive-types/02-primitive-types.js
@@ -0,0 +1,14 @@
+var intNoDecimal = 10;
+var numWithDecimal = 10.12;
+//both int and floats are just "number"s in typescript
+var total = intNoDecimal + 10;
+console.log("Total is =", total);
+var title = "TypeScript Bootcamp";
+var subtitle = "Learn the language fundamentals";
+var fullTitle = title + ": " + subtitle;
+console.log(fullTitle);
+var published = true;
+if (published) {
+    console.log("The course is published");
+}
+;
---
+++ fundamentals/02-primitive-types/02-primitive-types.ts
@@ -0,0 +1,23 @@
+const intNoDecimal: number = 10;
+const numWithDecimal: number = 10.12;
+//both int and floats are just "number"s in typescript
+
+const total = intNoDecimal + 10;
+
+console.log("Total is =", total);
+
+let title: string = "TypeScript Bootcamp";
+
+let subtitle: string = "Learn the language fundamentals";
+
+const fullTitle: string = title + ": " + subtitle;
+
+console.log(fullTitle);
+
+let published: boolean = true;
+
+if (published) {
+
+    console.log("The course is published");
+
+};
