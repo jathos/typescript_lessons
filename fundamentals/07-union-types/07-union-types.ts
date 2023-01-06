210cc6c8b0c0d56debcfda8dd87c22a146e2a97e
jbellon <josh@joshbellon.com>
2023-01-05 16:34:32 -0800

    finished lesson 'typescript non null assertion operator'

--- fundamentals/07-union-types/07-union-types.ts
+++ fundamentals/07-union-types/07-union-types.ts
@@ -15,3 +15,16 @@ const keys: (number | string)[] = [1000, "201e72bb-49e3-40ef-8331-b7f3e7d947f8"]
 
 let courseId: number | null = null;
 
+
+//strictNullChecks compiler flag:
+//older versions of TypeScript would allow you to assign null to variables even if it is not defined as an acceptable type
+//--strictNullChecks flag would mark that as an error
+
+
+let courseId2: number | null;
+
+courseId2.toString(); //this would throw an error if compiled with strict null checks
+//in order to avoid that error the above code could be written as:
+//courseId2!.toString()
+//using the ! will tell the compiler that you are sure this variable is NOT null and it is ok to ignore it during a strict null check
+
