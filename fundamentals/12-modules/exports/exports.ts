4d4bc2a9d6ad0977cd4b639a3b7387ca93eb562c
jbellon <josh@joshbellon.com>
2023-01-16 11:21:04 -0800

    finished lesson 'typescript re-exports'

--- fundamentals/12-modules/exports/exports.ts
+++ fundamentals/12-modules/exports/exports.ts
@@ -1,7 +1,17 @@
 export const PAGE_SIZE = 100;
 
+//you can even export and import Types
 export type Course = {
     readonly title: string,
     subtitle: string,
     lessonsCount?: number
 };
+
+//below import is a "re-export".  it is the same variable that was originally exported as PAGE_SIZE to imports.ts
+//then, in imports.ts it was exported again as the new variable name MAX_PAGE_SIZE using the "as" keyword
+
+//this feature can be useful if you want to collect all of your exports from different files into a single
+//exports file and them re-export them again.  that way you will only need to write a single import line to get
+//access to a all of your different exports in any modules that might need them
+import { MAX_PAGE_SIZE } from "../imports/imports";
+const newExport = MAX_PAGE_SIZE;
--- fundamentals/12-modules/imports/imports.ts
+++ fundamentals/12-modules/imports/imports.ts
@@ -2,3 +2,7 @@ import { PAGE_SIZE, Course } from "../exports/exports";
 
 const pageSize = PAGE_SIZE;
 console.log(pageSize);
+
+export {
+    PAGE_SIZE as MAX_PAGE_SIZE
+};
